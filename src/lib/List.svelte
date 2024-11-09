1a93807514f4438f6386a88ccf437f2d726df844
Jack Robinson <jrobinson@ksu.edu>
2024-11-08 21:43:15 -0600

    Initial Commit

---
+++ src/lib/List.svelte
@@ -0,0 +1,7 @@
+<script>
+	export let contents = [];
+</script>
+
+{#each contents as content}
+	<div>{content.id} | {content.name}</div>
+{/each}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+	import List from '../lib/List.svelte';
+
+	let listContents = $state([
+		{ id: 0, name: 'Jack R' },
+		{ id: 1, name: 'Dean B' }
+	]);
+
+	let nameInput = $state('');
+
+	const addPerson = () => {
+		listContents.push({ id: listContents.length, name: nameInput });
+		nameInput = '';
+	};
+</script>
+
+<div>
+	A List
+	<div>
+		<List contents={listContents} />
+	</div>
+	<div>
+		<input name="name" bind:value={nameInput} />
+		<button onclick={addPerson}>Add</button>
+	</div>
+</div>
+
+<style>
+</style>
