23e1604d232c040a560c5a83cb0f9c7af5537490
Jack Robinson <jrobinson@ksu.edu>
2024-11-08 21:59:39 -0600

    Styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,14 +14,17 @@
 	};
 </script>
 
-<div>
+<div class="h-full w-full">
 	A List
-	<div>
+	<div class="grid grid-cols-2 gap-4">
 		<List contents={listContents} />
 	</div>
 	<div>
-		<input name="name" bind:value={nameInput} />
-		<button onclick={addPerson}>Add</button>
+		<input class="p-2" name="name" bind:value={nameInput} />
+		<button
+			class="bg-blue-700 rounded p-2 border-none text-white shadow shadow-black hover:bg-blue-600 cursor-pointer"
+			onclick={addPerson}>Add</button
+		>
 	</div>
 </div>
 
